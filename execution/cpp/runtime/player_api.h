2ce28a0b77a7c0921ac1da79b39eff8576ee5278
breck768 <breckj768@gmail.com>
2026-03-31 10:36:08 -0500

    Initial commit but it does what i want it to

---
+++ execution/cpp/runtime/player_api.h
@@ -0,0 +1,15 @@
+//
+// Created by Emily Tew on 3/19/26.
+//
+
+#ifndef PLAYER_API_H
+#define PLAYER_API_H
+
+void move();
+void turn_left();
+void turn_right();
+bool front_is_clear();
+void pick_object();
+void put_object();
+
+#endif // PLAYER_API_H
