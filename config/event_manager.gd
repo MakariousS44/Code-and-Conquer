a6e76b849ad52d1a38ba43ccbed827a83d284d3e
MakariousS44 <mm2es@mtmail.mtsu.edu>
2026-04-21 20:25:10 -0500

    removed the last warnings

--- config/event_manager.gd
+++ config/event_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
 # Define global signal for different scenes to communicate
+@warning_ignore("unused_signal")
 signal rotate_camera_right
+@warning_ignore("unused_signal")
 signal rotate_camera_left
